509fa96105f1e83343697cfcfba140d35cfacc77
Jo <johj703@nate.com>
2024-10-23 17:48:00 +0900

    del: Database 삭제

--- src/utils/supabase/client.ts
+++ src/utils/supabase/client.ts
@@ -1,8 +1,7 @@
 import { createBrowserClient } from "@supabase/ssr";
-import { Database } from "@/lib/type/database.types";
 
 export const createClient = () =>
-  createBrowserClient<Database>(process.env.NEXT_PUBLIC_SUPABASE_URL!, process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!, {
+  createBrowserClient(process.env.NEXT_PUBLIC_SUPABASE_URL!, process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!, {
     auth: {
       persistSession: true, // 세션 지속
       detectSessionInUrl: true // URL에서 세션을 감지
