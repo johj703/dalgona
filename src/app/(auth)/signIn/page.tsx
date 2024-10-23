7b02432b9dd74e1ad34943aaa86f655c106a3e68
Jo <johj703@nate.com>
2024-10-23 22:03:33 +0900

    fix: 로그인 페이지 jsx 간편 로그인(소셜 로그인)제외하고 완성

--- src/app/(auth)/signIn/page.tsx
+++ src/app/(auth)/signIn/page.tsx
@@ -1,5 +1,26 @@
-const signInPage = () => {
-  return <div>page</div>;
-};
+import Link from "next/link";
+import Checkbox from "../../../components/signIn/CheckBox";
 
-export default signInPage;
+export default function signInPage() {
+  return (
+    <div>
+      <div>
+        <input className="border border-black" placeholder="이메일" />
+      </div>
+      <div>
+        <input className="border border-black" placeholder="비밀번호" />
+      </div>
+      <div className="flex">
+        <Checkbox />
+        <p>자동 로그인</p>
+      </div>
+      <button className="border border-black">로그인</button>
+      <div className="space-x-2">
+        <Link href="">아이디 찾기</Link>
+        <Link href="">비밀번호 찾기</Link>
+        <Link href="">회원가입</Link>
+      </div>
+      <hr />
+    </div>
+  );
+}
