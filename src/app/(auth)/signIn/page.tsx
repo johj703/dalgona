523eb04b14a2173f270f67b3e4a36130c830c4a2
Jo <johj703@nate.com>
2024-10-24 15:12:51 +0900

    fix: 가짜 사용자 데이터, 로그인 처리 함수 추가

--- src/app/(auth)/signIn/page.tsx
+++ src/app/(auth)/signIn/page.tsx
@@ -2,8 +2,35 @@
 
 import Link from "next/link";
 import Checkbox from "../../../components/signIn/CheckBox";
+import { useRouter } from "next/router";
+import { useState } from "react";
 
 export default function signInPage() {
+  const [email, setEamil] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+  const router = useRouter();
+
+  // 가짜 사용자 데이터(supabase와 연결하면 이 부분은 supabase API로 변경)
+  const fakeUser = {
+    email: "user@example.com",
+    password: "password123"
+  };
+
+  // 로그인 처리 함수
+  const handleSignIn = (e: React.FormEvent) => {
+    e.preventDefault();
+
+    // 입력한 이메일과 비밀번호가 가짜 사용자 데이터와 일치하는지 확인
+    if (email === fakeUser.email && password === fakeUser.password) {
+      console.log("로그인 성공");
+      // 로그인 성공 후 리다이렉트
+      router.push("/dashboard");
+    } else {
+      setError("아이디 또는 비밀번호가 올바르지 않습니다.");
+    }
+  };
+
   return (
     <div>
       <div className="space-y-2">
