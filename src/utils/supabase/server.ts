6db88bd59313aa45f87877cd2d4de88db16670ed
Jo <johj703@nate.com>
2024-10-28 10:57:15 +0900

    fix: server.ts 오류 수정

--- src/utils/supabase/server.ts
+++ src/utils/supabase/server.ts
@@ -1,7 +1,7 @@
 import { createServerClient } from "@supabase/ssr";
 import { cookies } from "next/headers";
-export const createClient = async () => {
-  const cookieStore = await cookies();
+export const createClient =  () => {
+  const cookieStore = cookies();
   return createServerClient(process.env.NEXT_PUBLIC_SUPABASE_URL!, process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!, {
     cookies: {
       getAll() {
