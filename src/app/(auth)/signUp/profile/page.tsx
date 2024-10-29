ca3d2c50bf548084db1afd2883f297026e87d852
Jo <johj703@nate.com>
2024-10-29 14:24:45 +0900

    fix: 프로필 작성 페이지에 필요한 함수 작성

--- src/app/(auth)/signUp/profile/page.tsx
+++ src/app/(auth)/signUp/profile/page.tsx
@@ -9,5 +9,19 @@ export default function saveUserProfilePage() {
   const [birthMonth, setBirthMonth] = useState("");
   const [gender, setGender] = useState("");
 
+  // 프로필 이미지 변경 시 미리보기 URL 설정
+  const handleProfileImageChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (e.target.files && e.target.files[0]) {
+      const file = e.target.files[0];
+      setProfileImage(file);
+      setProfileImagePreview(URL.createObjectURL(file));
+    }
+  };
+
+  // 프로필 저장 처리 함수
+  const handleSaveProfile = () => {
+    console.log("프로필 저장 : ", { profileImage, birthYear, birthMonth, gender });
+  };
+
   return <div>page</div>;
 }
