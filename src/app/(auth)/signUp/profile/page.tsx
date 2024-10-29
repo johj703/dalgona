112a1f697454408512466d67e41ad35a69d18d37
Jo <johj703@nate.com>
2024-10-29 13:31:59 +0900

    fix: 프로필 작성 페이지에 필요한 state 생성

--- src/app/(auth)/signUp/profile/page.tsx
+++ src/app/(auth)/signUp/profile/page.tsx
@@ -1,3 +1,13 @@
+"use client";
+
+import { useState } from "react";
+
 export default function saveUserProfilePage() {
+  const [profileImage, setProfileImage] = useState<File | null>(null);
+  const [profileImagePreview, setProfileImagePreview] = useState<string | null>(null);
+  const [birthYear, setBirthYear] = useState("");
+  const [birthMonth, setBirthMonth] = useState("");
+  const [gender, setGender] = useState("");
+
   return <div>page</div>;
 }
