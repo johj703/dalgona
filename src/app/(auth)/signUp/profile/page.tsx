71e2ce1d75d1f2fe72a94f36e0cacb5aac0c8049
Jo <johj703@nate.com>
2024-10-31 17:22:04 +0900

    fix: import 문에서 확장자 삭제

--- src/app/(auth)/signUp/profile/page.tsx
+++ src/app/(auth)/signUp/profile/page.tsx
@@ -6,7 +6,7 @@ import { z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
 import Image from "next/image";
 import { useRouter } from "next/navigation";
-import supabase from "@/utils/supabase/client.ts";
+import supabase from "@/utils/supabase/client";
 
 // 입력 유효성 검사를 위해서 Zod 스키마 정의
 const profileSchema = z.object({
