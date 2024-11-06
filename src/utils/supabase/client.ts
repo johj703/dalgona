d2358bb534f77b35c73ee5d0a5a2320ffa418778
Jo <johj703@nate.com>
2024-11-06 16:20:16 +0900

    fix: Supabase/client.ts에서 함수의 이름을 supabase로 변경

--- src/utils/supabase/client.ts
+++ src/utils/supabase/client.ts
@@ -1,13 +1,14 @@
 import { createBrowserClient } from "@supabase/ssr";
 
-export const createClient = () =>
-  createBrowserClient(process.env.NEXT_PUBLIC_SUPABASE_URL!, process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!, {
+export const supabase = createBrowserClient(
+  process.env.NEXT_PUBLIC_SUPABASE_URL!,
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+  {
     auth: {
       persistSession: true, // 세션 지속
       detectSessionInUrl: true // URL에서 세션을 감지
     }
-  });
+  }
+);
 
-const browserClient = createClient();
-
-export default browserClient;
+export default supabase;
