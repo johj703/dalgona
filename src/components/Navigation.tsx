6f4d8e090d95837fc9475cfcdceb738295198bf5
heerokj <jungheerok@gmail.com>
2024-11-09 00:10:49 +0900

    fix : 네비게이션 바 페이지 이동시 색 유지

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,6 +1,7 @@
 "use client";
 import Image from "next/image";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 import { useState } from "react";
 
 const NAVIGATION_LIST = ["/main", "/diary/write", "/library", "/mypage"];
@@ -11,7 +12,10 @@ const getNavigation = (index: string, isOn: string) => {
 };
 
 const Navigation = () => {
+  const params = usePathname();
   const [formData, setFormData] = useState("");
+  console.log("🚀 ~ Navigation ~ formData:", formData);
+
   return (
     <div className="fixed bottom-[-1px] left-0 right-0">
       <ul className="flex justify-between gap-[1px] self-stretch bg-[#FDF7F4]">
@@ -23,7 +27,7 @@ const Navigation = () => {
                 className="flex w-[98.5px] h-[52px] pt-[14px] pr-[41.5px] pl-[37px] pb-[18px] items-center border-[1px] border-[#FDF7F4] shrink-0"
                 onClick={() => setFormData(navi)}
               >
-                {formData === navi ? (
+                {params === navi ? (
                   <Image src={getNavigation(navi, "on")} alt={navi} width={24} height={24} />
                 ) : (
                   <Image src={getNavigation(navi, "off")} alt={navi} width={24} height={24} />
