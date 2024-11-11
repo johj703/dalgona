2cd728c25807e29a2a552e935bfbd7e5a346f5e5
Jo <johj703@nate.com>
2024-11-11 22:05:35 +0900

    add: 내 정보 수정 페이지에서 supabase 할당, userData state 생성

---
+++ src/app/mypage/editprofile/page.tsx
@@ -0,0 +1,11 @@
+import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
+import { useState } from "react";
+
+const EditProfilePage = () => {
+  const supabase = createClientComponentClient();
+  const [userData, setUserData] = useState({ nickname: "", profilePic: "" });
+
+  return <div>page</div>;
+};
+
+export default EditProfilePage;
