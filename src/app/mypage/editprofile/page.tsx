fa78b34bd522b0b30241ed10ecb269e9bf43b50f
Jo <johj703@nate.com>
2024-11-11 22:47:42 +0900

    fix: useEffect로 user의 정보를 가지고 오는 함수 작성

--- src/app/mypage/editprofile/page.tsx
+++ src/app/mypage/editprofile/page.tsx
@@ -1,11 +1,28 @@
 "use client";
 
 import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
-import { useState } from "react";
+import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
 
 const EditProfilePage = () => {
   const supabase = createClientComponentClient();
-  const [userData, setUserData] = useState({ nickname: "", profilePic: "" });
+  const [nickname, setNickname] = useState("");
+  const [profileImage, setProfileImage] = useState("");
+  const router = useRouter();
+  const DEFAULT_IMAGE = "/icons/default-profile.png";
+
+  useEffect(() => {
+    const fetchUserData = async () => {
+      const {
+        data: { user }
+      } = await supabase.auth.getUser();
+      if (user) {
+        setNickname(user.nickname || "");
+        setProfileImage(user.profile_image || DEFAULT_IMAGE);
+      }
+    };
+    fetchUserData();
+  }, []);
 
   return <div>page</div>;
 };
