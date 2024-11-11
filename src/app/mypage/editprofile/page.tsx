28b3e1e86d1fd1d8b75305d5ac045ade48ad9795
Jo <johj703@nate.com>
2024-11-11 22:50:44 +0900

    fix: 변경사항을 저장하는 함수 작성

--- src/app/mypage/editprofile/page.tsx
+++ src/app/mypage/editprofile/page.tsx
@@ -24,6 +24,23 @@ const EditProfilePage = () => {
     fetchUserData();
   }, []);
 
+  const handleSave = async () => {
+    const { error } = await supabase
+      .from("profiles")
+      .update({
+        nickname,
+        profile_image: profileImage
+      })
+      .eq("id", user.id);
+
+    if (error) {
+      alert("프로필 업데이트에 실패했습니다.");
+    } else {
+      alert("프로필이 업데이트가 완료되었습니다.");
+      router.push("/mypage"); // 수정 후 mypage로 이동
+    }
+  };
+
   return <div>page</div>;
 };
 
