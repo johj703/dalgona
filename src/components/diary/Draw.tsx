7c057255293e9912d0eda5006ffd5ca11f7748d1
PJH-FE <sjrmd789@gmail.com>
2024-11-12 20:36:21 +0900

    update: canvas range바 추가

--- src/components/diary/Draw.tsx
+++ src/components/diary/Draw.tsx
@@ -103,20 +103,33 @@ const Draw = ({ POST_ID, setFormData, formData, setGoDraw, goDraw }: DrawProps) 
           )}
         </button>
 
-        {tool === "pen" && (
+        {tool === "pen" || tool === "eraser" ? (
           <div className="absolute bottom-full flex items-center gap-4 w-full py-[22px] px-8 rounded-tl-lg rounded-tr-lg overflow-x-auto scrollbar-hide bg-[#404040]">
-            <input
-              type="range"
-              name="lineWidth"
-              id="lineWidth"
-              min={1}
-              max={20}
-              value={lineCustom.lineWidth}
-              step={1}
-              onChange={(e) => handleChangeCustom(e)}
-            />
+            <div className="relative w-full h-4 bg-[rgba(255,255,255,0.5)] rounded-[10px]">
+              <span
+                className={`relative block h-4 bg-[#2BCFF2] transition rounded-l-[10px]`}
+                style={{ width: `${((Number(lineCustom.lineWidth) - 1) / 30) * 100}%` }}
+              >
+                <label
+                  htmlFor="lineWidth"
+                  className="absolute top-1/2 right-0 -translate-y-1/2 translate-x-6 w-7 h-7 bg-white rounded-full"
+                ></label>
+              </span>
+              <input
+                type="range"
+                name="lineWidth"
+                id="lineWidth"
+                min={1}
+                max={30}
+                value={lineCustom.lineWidth}
+                step={1}
+                onChange={(e) => handleChangeCustom(e)}
+                className="absolute top-1/2 left-0 -translate-y-1/2 w-full h-7 appearance-none opacity-0"
+              />
+            </div>
           </div>
-        )}
+        ) : null}
+
         {tool === "pallete" && (
           <Pallete lineCustom={lineCustom} setLineCustom={setLineCustom} handleChangeCustom={handleChangeCustom} />
         )}
