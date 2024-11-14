b7d91b481aa1e77a569b343540192b40b146a696
PJH-FE <sjrmd789@gmail.com>
2024-11-15 01:54:40 +0900

    style: 마이페이지 메인 수정

--- src/lib/mypage/getMyDrawing.ts
+++ src/lib/mypage/getMyDrawing.ts
@@ -1,6 +1,7 @@
 import browserClient from "@/utils/supabase/client";
 
-export const getMyDrawing = async (user_id: string) => {
+export const getMyDrawing = async (user_id: string, device: string) => {
+  const maxRange = device === "pc" ? 5 : 2;
   try {
     const { data, count } = await browserClient
       .from("diary")
@@ -8,7 +9,7 @@ export const getMyDrawing = async (user_id: string) => {
       .eq("user_id", user_id)
       .neq("draw", null)
       .order("created_at", { ascending: false })
-      .range(0, 2);
+      .range(0, maxRange);
 
     return { data, count };
   } catch (error) {
