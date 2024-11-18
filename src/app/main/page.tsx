6497db4b68dabc12e89469acf16f6bad777efd74
heerokj <jungheerok@gmail.com>
2024-11-18 17:15:31 +0900

    style : 임시헤더

--- src/app/main/page.tsx
+++ src/app/main/page.tsx
@@ -1,9 +1,11 @@
 import SelectFeedCalendar from "@/components/main/SelectFeedCalendar";
 import Navigation from "@/components/Navigation";
+import Header from "@/components/layout/Header";
 
 const MainPage = async () => {
   return (
     <>
+      <Header />
       <div className="lg:flex lg:flex-col lg:justify-center lg:items-center ">
         <SelectFeedCalendar />
         <Navigation />
