e33c44fafdfd1efe1d653310f8321bada603190b
heerokj <jungheerok@gmail.com>
2024-11-20 21:04:18 +0900

    fix: 불러오는 데이터 개수 수정 5->6 style :  top버튼 웹에서 삭제

--- src/components/TopButton.tsx
+++ src/components/TopButton.tsx
@@ -12,7 +12,7 @@ const TopButton = () => {
     <>
       <div
         onClick={handleTop}
-        className="flex w-[56px] h-[56px] p-[16px] items-center gap-[10px] fixed z-50 right-[4px] bottom-[61px] rounded-full bg-[#E89080] cursor-pointer"
+        className="flex w-[56px] h-[56px] p-[16px] items-center gap-[10px] fixed z-50 right-[4px] bottom-[61px] rounded-full bg-[#E89080] cursor-pointer lg:hidden"
       >
         <div className="flex w-[24px] h-[24px] justify-center items-center gap-[24px] shrink-0">
           <Image src="/icons/arrow-top.svg" width={12} height={12} alt="arrow-top" className="shrink-0" />
